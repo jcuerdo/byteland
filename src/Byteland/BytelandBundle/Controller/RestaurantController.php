799b225c5382bf911366cff9d2188ecb019a2a11
Jorge Cuerdo <jocual@gmail.com>
2015-03-17 07:50:08 +0100

    Add restaurant crud classes

--- src/Byteland/BytelandBundle/Controller/RestaurantController.php
+++ src/Byteland/BytelandBundle/Controller/RestaurantController.php
@@ -2,45 +2,47 @@
 
 namespace Byteland\BytelandBundle\Controller;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use Byteland\BytelandBundle\Entity\Person;
-use Byteland\BytelandBundle\Form\PersonType;
+use Byteland\BytelandBundle\Entity\Restaurant;
+use Byteland\BytelandBundle\Form\RestaurantType;
 
 /**
- * Person controller.
+ * Restaurant controller.
  *
  */
-class PersonController extends Controller
+class RestaurantController extends Controller
 {
 
     /**
-     * Lists all Person entities.
+     * Lists all Restaurant entities.
      *
      */
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
 
-        $entities = $em->getRepository('BytelandBundle:Person')->findAll();
+        $entities = $em->getRepository('BytelandBundle:Restaurant')->findAll();
+
         foreach($entities as $key => $value)
         {
             $entities[$key] = array('id' => $value->getId(),'name' => $value->getName());
         }
         $response = new JsonResponse($entities,JsonResponse::HTTP_OK,array('Content-Type' => 'application/json'));
+
         return $response;
     }
     /**
-     * Creates a new Person entity.
+     * Creates a new Restaurant entity.
      *
      */
     public function createAction(Request $request)
     {
-        $entity = new Person();
+        $entity = new Restaurant();
 
         $entity->setName($request->get('name'));
+        $entity->setMaxAcceptedPeople($request->get('max_accepted_people'));
 
         $em = $this->getDoctrine()->getManager();
         $em->persist($entity);
@@ -51,22 +53,20 @@ class PersonController extends Controller
     }
 
     /**
-     * Finds and displays a Person entity.
+     * Finds and displays a Restaurant entity.
      *
      */
     public function showAction($id)
     {
         $em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('BytelandBundle:Person')->find($id);
-
+        $entity = $em->getRepository('BytelandBundle:Restaurant')->find($id);
 
         if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Person.');
+            throw $this->createNotFoundException('Unable to find Restaurant.');
         }
 
-
-        $entity = array('id' => $entity->getId(),'name' => $entity->getName());
+        $entity = array('id' => $entity->getId(),'name' => $entity->getName(),'max_accepted_people' => $entity->getMaxAcceptedPeople());
 
         $response = new JsonResponse($entity,JsonResponse::HTTP_OK,array('Content-Type' => 'application/json'));
 
@@ -75,17 +75,23 @@ class PersonController extends Controller
     }
 
     /**
-     * Edits an existing Person entity.
+     * Edits an existing Restaurant entity.
      *
      */
     public function updateAction(Request $request, $id)
     {
         parse_str($request->getContent(), $params);
+
         $em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('BytelandBundle:Person')->find($id);
+        $entity = $em->getRepository('BytelandBundle:Restaurant')->find($id);
 
         $entity->setName($params['name']);
+        $entity->setMaxAcceptedPeople($params['max_accepted_people']);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Restaurant.');
+        }
 
         $em = $this->getDoctrine()->getManager();
         $em->persist($entity);
@@ -94,18 +100,22 @@ class PersonController extends Controller
         $response = new JsonResponse('OK',JsonResponse::HTTP_OK,array('Content-Type' => 'application/json'));
 
         return $response;
+
     }
     /**
-     * Deletes a Person entity.
+     * Deletes a Restaurant entity.
      *
      */
-    public function deleteAction($id)
+    public function deleteAction(Request $request, $id)
     {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
         $em = $this->getDoctrine()->getManager();
-        $entity = $em->getRepository('BytelandBundle:Person')->find($id);
+        $entity = $em->getRepository('BytelandBundle:Restaurant')->find($id);
 
         if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Person entity.');
+            throw $this->createNotFoundException('Unable to find Restaurant entity.');
         }
 
         $em->remove($entity);
@@ -113,5 +123,22 @@ class PersonController extends Controller
         $response = new JsonResponse('OK',JsonResponse::HTTP_OK,array('Content-Type' => 'application/json'));
 
         return $response;
+    }
+
+    /**
+     * Creates a form to delete a Restaurant entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('restaurant_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
     }
 }
