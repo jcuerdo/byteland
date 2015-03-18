675e58671d1b91f1d6bf72e9dfe33c1ab03900d5
Jorge Cuerdo <jocual@gmail.com>
2015-03-18 22:26:55 +0100

    Add booking to the domain

--- src/Byteland/BytelandBundle/Controller/BookingController.php
+++ src/Byteland/BytelandBundle/Controller/BookingController.php
@@ -2,10 +2,11 @@
 
 namespace Byteland\BytelandBundle\Controller;
 
+use Byteland\BytelandDomain\Model\Restaurant;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-use Byteland\BytelandBundle\Entity\Booking;
+use \Byteland\BytelandDomain\Model\Person;
+use \Byteland\BytelandDomain\Model\Booking;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
@@ -20,41 +21,24 @@ class BookingController extends Controller
      */
     public function createAction(Request $request)
     {
-        if(!$request->get('id_restaurant') || !$request->get('id_person') || !$request->get('date')) {
-            $response = new JsonResponse('ERROR',JsonResponse::HTTP_BAD_REQUEST ,array('Content-Type' => 'application/json'));
-            return $response;
+        try{
+            if(!$request->get('id_restaurant') || !$request->get('id_person') || !$request->get('date')) {
+                $response = new JsonResponse('ERROR',JsonResponse::HTTP_BAD_REQUEST ,array('Content-Type' => 'application/json'));
+                return $response;
+            }
+
+            $restaurant = new Restaurant($request->get('id_restaurant'),'');
+            $person = new Person($request->get('id_person'),'');
+            $booking = new Booking(null,new \DateTime($request->get('date')), $restaurant, $person);
+            $booking_repository = $this->get('booking.repository');
+
+            $booking_repository->add($booking);
+
+            $response = new JsonResponse('OK',JsonResponse::HTTP_CREATED ,array('Content-Type' => 'application/json'));
         }
-
-        $entity = new Booking();
-
-        $em = $this->getDoctrine()->getManager();
-
-        $entity->setDate(new \DateTime($request->get('date')));
-        
-        $restaurant = $em->getRepository('BytelandBundle:Restaurant')->find($request->get('id_restaurant'));
-
-        $bookings =  $em->getRepository('BytelandBundle:Booking')->findByDate($restaurant, new \DateTime($request->get('date')));
-
-        $availability =  $em->getRepository('BytelandBundle:Availability')->findByDate($restaurant, new \DateTime($request->get('date')));
-
-        if(count($bookings) >= $restaurant->getMaxAcceptedPeople()) {
-            $response = new JsonResponse('RESTAURANT FULL',JsonResponse::HTTP_PRECONDITION_REQUIRED ,array('Content-Type' => 'application/json'));
-            return $response;
+        catch(\Exception $e) {
+            $response = new JsonResponse($e->getMessage(),JsonResponse::HTTP_CREATED ,array('Content-Type' => 'application/json'));
         }
-        if(count($availability) == 0){
-            $response = new JsonResponse('RESTAURANT NOT AVAILABLE',JsonResponse::HTTP_PRECONDITION_REQUIRED ,array('Content-Type' => 'application/json'));
-            return $response;
-        }
-
-        $person = $em->getRepository('BytelandBundle:Person')->find($request->get('id_person'));
-        $entity->setRestaurant($restaurant);
-        $entity->setPerson($person);
-
-        $em->persist($entity);
-        $em->flush();
-
-        $response = new JsonResponse('OK',JsonResponse::HTTP_CREATED ,array('Content-Type' => 'application/json'));
-        
         return $response;
     }
 }
--- src/Byteland/BytelandDomain/Persistence/Doctrine/DoctrineBookingRepository.php
+++ src/Byteland/BytelandDomain/Persistence/Doctrine/DoctrineBookingRepository.php
@@ -4,6 +4,7 @@ namespace Byteland\BytelandDomain\Persistence\Doctrine;
 
 use Byteland\BytelandDomain\Model\Booking;
 use Byteland\BytelandDomain\Persistence\BookingRepository;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 class DoctrineBookingRepository extends DoctrineGenericRepository implements BookingRepository
 {
@@ -11,5 +12,29 @@ class DoctrineBookingRepository extends DoctrineGenericRepository implements Boo
     {
         parent::__construct($em);
     }
-    public function add(Booking $booking){}
+    public function add(Booking $booking)
+    {
+        $doctrine_booking = new \Byteland\BytelandBundle\Entity\Booking();
+        $doctrine_booking->setDate($booking->getDate());
+
+        $doctrine_restaurant = $this->em->getRepository('BytelandBundle:Restaurant')->find($booking->getRestaurant()->getId());
+        $doctrine_bookings =  $this->em->getRepository('BytelandBundle:Booking')->findByDate($doctrine_restaurant, $booking->getDate());
+
+        $doctrine_availability =  $this->em->getRepository('BytelandBundle:Availability')->findByDate($doctrine_restaurant, $booking->getDate());
+
+        if(count($doctrine_bookings) >= $doctrine_restaurant->getMaxAcceptedPeople()) {
+            throw new \Exception('RESTAURANT FULL');
+        }
+        if(count($doctrine_availability) == 0){
+            throw new \Exception('RESTAURANT NOT AVAILABLE');
+        }
+
+        $person = $this->em->getRepository('BytelandBundle:Person')->find($booking->getPerson()->getId());
+
+        $doctrine_booking->setRestaurant($doctrine_restaurant);
+        $doctrine_booking->setPerson($person);
+
+        $this->em->persist($doctrine_booking);
+        $this->em->flush();
+    }
 }
